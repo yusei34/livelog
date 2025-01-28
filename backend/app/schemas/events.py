934ee6f9ffadb42f6da6a2fb1cd026f2331e3e80
yusei <matsu.yusei3414@gmail.com>
2025-01-29 05:57:49 +0900

    スキーマの共通事項をまとめてベースクラスを定義し,リクエスト型のスキーマを定義,およびレスポンス型の不要部分を削除

--- backend/app/schemas/events.py
+++ backend/app/schemas/events.py
@@ -1,11 +1,16 @@
 import datetime
 from pydantic import BaseModel, Field
 
-
-class Events(BaseModel):
-    id:int 
-    name:str
-    venue:str
+class EventsBase(BaseModel):
+    name:str = Field(str, examples='ARABAKI ROCK FEST')
+    venue:str 
     date:datetime.date
     ticket_status:str | None = None #あとでenumに修正
     expense:int | None = None
+    
+
+class Events(EventsBase):
+    id:int 
+
+class EventsCreate(EventsBase):
+    pass
