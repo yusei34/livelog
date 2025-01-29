4cf9ae912b0687432b5eadf4b3c0b201ea812d15
yusei <matsu.yusei3414@gmail.com>
2025-01-29 22:09:53 +0900

    スキーマのEventsBaseクラスのデータ定義を修正,POST,PUT,DELETEのパスオペレーション関数を定義

--- backend/app/schemas/events.py
+++ backend/app/schemas/events.py
@@ -1,19 +1,27 @@
-import datetime
+from datetime import date
 from pydantic import BaseModel, Field
+from enum import Enum
+
+class TicketStatus(str,Enum):
+    ON_APPLICATION = '申込中'
+    PURCHASED = '購入済'
+    REJECTION = '落選' 
+
 
 class EventsBase(BaseModel):
-    name:str = Field(str, examples='ARABAKI ROCK FEST')
-    venue:str 
-    date:datetime.date
-    ticket_status:str | None = None #あとでenumに修正
+    name:str = Field(..., examples=['ARABAKI ROCK FEST'])
+    venue:str = Field(..., examples=['国営みちのく杜の湖畔公園'])
+    date:date
+    ticket_status:TicketStatus | None = None #あとでenumに修正
     expense:int | None = None
     
-#GETのレスポンス
+#レスポンス
 class Events(EventsBase):
     id:int 
-
+    
+    class Config:
+        orm_mode = True
+    
 class EventsCreate(EventsBase):
     pass
 
-class Config:
-    orm_mode = True
