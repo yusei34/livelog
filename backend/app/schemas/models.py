a5df90a798a0ad2a071af744cfbb4d20aa480d99
yusei <matsu.yusei3414@gmail.com>
2025-02-16 23:04:48 +0900

    中間テーブルモデルEventActorLinkを定義

--- backend/app/schemas/models.py
+++ backend/app/schemas/models.py
@@ -9,56 +9,61 @@ class TicketStatus(str,Enum):
     PURCHASED = '購入済'
     REJECTION = '落選' 
 
+class EventActorLink(SQLModel):
+    event_id:uuid.UUID | None = Field(default=None, foreign_key="event.id", primary_key=True)
+    actor_id:uuid.UUID | None = Field(default=None, foreign_key="actor.id", primary_key=True)
+
 # EventsテーブルおよびCRUD操作用のベーススキーマ
-class EventsBase(SQLModel):
+class EventBase(SQLModel):
     event_name: str = Field(description='ライブイベントのタイトル',examples=['ARABAKI ROCK FEST'])
     venue: str = Field(description='ライブイベントの会場名', examples=['国営みちのく杜の湖畔公園'])
     date: date
     ticket_status: TicketStatus | None = Field(default=None)
 
 # GET イベント取得(出力用)
-class EventsPublic(EventsBase):
+class EventPublic(EventBase):
     id: uuid.UUID
 
 # POST ライブイベント登録(入力用)
-class EventsCreate(EventsBase):
+class EventCreate(EventBase):
     pass
 
 # PUT ライブイベント更新(修正)
-class EventsUpdate(EventsBase):
+class EventUpdate(EventBase):
     event_name: str | None = Field(default=None)
     venue: str | None = Field(default=None)
     date: date | None = Field(default=None)
 
-# Eventsテーブルのモデル定義
-class Events(EventsBase, table=True):
+# Eventテーブルのモデル定義
+class Event(EventBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     event_name: str = Field(nullable=False, max_length=255)
     venue: str = Field(nullable=False, max_length=255)
     expense_id : uuid.UUID | None = Field(foreign_key="expenses.id", ondelete="CASCADE")
     expenses: Expenses | None = Relationship(back_populates="events")
-    actors: EventActor | None = Relationship(back_populates="events")
-
+    actors: list["Actor"] = Relationship(back_populates="event", link_model=EventActorLink)
 # ActorsテーブルおよびCRUD操作用のベーススキーマ
-class ActorsBase(SQLModel):
+class ActorBase(SQLModel):
     name: str
     favorite: bool = Field(default=False)
 
 # GET アーティスト情報取得(出力用)
-class ActorsPublic(ActorsBase):
+class ActorsPublic(ActorBase):
     id: uuid.UUID
 
 # POST アーティスト登録(入力用)
-class ActorsCreate(ActorsBase):
+class ActorsCreate(ActorBase):
     pass
 
 # PUT 更新用
-class ActorsUpdate(ActorsBase):
+class ActorsUpdate(ActorBase):
     name: str | None = Field(default=None)
 
-class Actors(ActorsBase, table=True):
+# Actorsテーブルのモデル定義
+class Actor(ActorBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str = Field(nullable=False, max_length=255)
-    events: EventActor | None = Relationship(back_populates="actors")
-
     
+    events: list["Event"] = Relationship(back_populates="actor", link_model=EventActorLink)
+
+
