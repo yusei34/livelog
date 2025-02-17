a8b140da09033219df9b8e29400f7d9358a5ec5c
yusei <matsu.yusei3414@gmail.com>
2025-02-17 23:04:31 +0900

    共通メッセージおよび認証関連のモデルを定義

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,15 +10,15 @@ class TicketStatus(str,Enum):
     REJECTION = '落選' 
 
 class EventActorLink(SQLModel):
-    event_id:uuid.UUID | None = Field(default=None, foreign_key="event.id", primary_key=True)
-    actor_id:uuid.UUID | None = Field(default=None, foreign_key="actor.id", primary_key=True)
+    event_id:uuid.UUID | None = Field(default= None, foreign_key= "event.id", primary_key= True)
+    actor_id:uuid.UUID | None = Field(default= None, foreign_key= "actor.id", primary_key= True)
 
 # EventsテーブルおよびCRUD操作用のベーススキーマ
 class EventBase(SQLModel):
-    event_name: str = Field(description='ライブイベントのタイトル',examples=['ARABAKI ROCK FEST'])
-    venue: str = Field(description='ライブイベントの会場名', examples=['国営みちのく杜の湖畔公園'])
+    event_name: str = Field(description= 'ライブイベントのタイトル',examples= ['ARABAKI ROCK FEST'])
+    venue: str = Field(description= 'ライブイベントの会場名', examples= ['国営みちのく杜の湖畔公園'])
     date: date
-    ticket_status: TicketStatus | None = Field(default=None)
+    ticket_status: TicketStatus | None = Field(default= None)
 
 # GET イベント取得(出力用)
 class EventPublic(EventBase):
@@ -30,24 +30,24 @@ class EventCreate(EventBase):
 
 # PUT ライブイベント更新(修正)
 class EventUpdate(EventBase):
-    event_name: str | None = Field(default=None)
-    venue: str | None = Field(default=None)
-    date: date | None = Field(default=None)
+    event_name: str | None = Field(default= None)
+    venue: str | None = Field(default= None)
+    date: date | None = Field(default= None)
 
 # Eventテーブルのモデル定義
 class Event(EventBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     event_name: str = Field(nullable=False, max_length=255)
     venue: str = Field(nullable=False, max_length=255)
-    expense_id : uuid.UUID | None = Field(foreign_key="expenses.id", ondelete="CASCADE")
+    expense_id : uuid.UUID | None = Field(foreign_key="expenses.id", ondelete= "CASCADE")
     
-    expense: list["Expense"]= Relationship(back_populates="events")
-    actors: list["Actor"] = Relationship(back_populates="events", link_model=EventActorLink)
+    expense: list["Expense"]= Relationship(back_populates= "events")
+    actors: list["Actor"] = Relationship(back_populates= "events", link_model= EventActorLink)
     
 # ActorsテーブルおよびCRUD操作用のベーススキーマ
 class ActorBase(SQLModel):
     name: str
-    favorite: bool = Field(default=False)
+    favorite: bool = Field(default= False)
 
 # GET アーティスト情報取得(出力用)
 class ActorsPublic(ActorBase):
@@ -66,7 +66,7 @@ class Actor(ActorBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str = Field(nullable=False, max_length=255)
     
-    events: list["Event"] = Relationship(back_populates="actor", link_model=EventActorLink)
+    events: list["Event"] = Relationship(back_populates="actors", link_model=EventActorLink)
 
 # ExpenseテーブルおよびCRUDのモデル
 class ExpenseBase(SQLModel):
@@ -92,4 +92,25 @@ class ExpenseUpdate(ExpenseBase):
 # Expenseテーブルのモデル定義
 class Expense(ExpenseBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    event_id: uuid.UUID | None = Field(default=None, foreign_key="events.id")
+    event_id: uuid.UUID | None = Field(default=None, foreign_key="event.id")
+
+# 共通メッセージおよび認証関連のモデル
+# Generic message
+class Message(SQLModel):
+    message: str
+
+
+# JSON payload containing access token
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+# Contents of JWT token
+class TokenPayload(SQLModel):
+    sub: str | None = None
+
+
+class NewPassword(SQLModel):
+    token: str
+    new_password: str = Field(min_length=8, max_length=40)
