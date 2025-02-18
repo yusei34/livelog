e8026f797c033c0b3e5cf8709c15215316535f6e
yusei <matsu.yusei3414@gmail.com>
2025-02-18 22:15:26 +0900

    基本のcrudにかかるendpointを定義した

--- backend/app/api/endpoints/events.py
+++ backend/app/api/endpoints/events.py
@@ -11,28 +11,66 @@ import backend.app.models as events_schema
 router = APIRouter(prefix="/events", tags=["events"])
 
 @router.get('/', response_model=list[EventPublic])
-async def read_events(
+def read_events(
     session: SessionDep, skip: int = 0, limit: int = Query(default=20, le=20)
 ) -> Any :
     """
-    Retrieve heroes.
+    Retrieve events.
     """
     events = session.exec(select(Event).offset(skip).limit(limit)).all()
     return events
+
+@router.get('/{event_id}', response_model= EventPublic)
+def read_event(
+    event_id: uuid.UUID, session:SessionDep
+) -> Any:
+    """
+    Get event by ID.
+    """
+    event = session.get(Event, event_id)
+    if not event:
+        raise HTTPException(status_code=404, detail='event is not found')
+    return event
+
+@router.post('/', response_model= EventPublic)
+def create_event(event_in: EventCreate, session:SessionDep) -> Any:
+    """
+    Create new event.
+    """
+    event = Event.model_validate(event_in)
+    session.add(event)
+    session.commit()
+    session.refresh(event)
+    return event
+
+@router.put('/{event_id}', response_model= EventPublic)
+def update_event(
+    event_id: uuid.UUID, event_in: EventUpdate, session: SessionDep
+) -> Any:
+    """
+    Update an event.
+    """
+    event = session.get(Event, event_id)
+    if not event:
+        raise HTTPException(status_code= 404, detail='event is not found')
+    update_data = event_id.model_dump(exclude_unset= True)
+    event.sqlmodel_update(update_data)
+    session.add(event)
+    session.commit()
+    session.refresh(event)
+    return event
     
+@router.delete('/{event_id}', response_model= Message)
+def delete_event(event_id: uuid.UUID, session: SessionDep) -> Any:
+    """
+    Delete an event.
+    """
+    event = session.get(Event, event_id)
+    if not event:
+        raise HTTPException(status_code= 404, detail='event is not found')
+    session.delete(event)
+    session.commit()
+    return Message(message='event deleted successfully')
 
-@router.post('/events',response_model=events_schema.Events)
-async def create_events(events_body: events_schema.EventsCreate):
-    return events_schema.Events(id=1,**events_body.dict())
 
-@router.get('/events/{events_id}',response_model = events_schema.Events)
-async def read_events(events_id: int):
-    return events_schema.Events
 
-@router.put('/events/{events_id}', response_model=events_schema.Events)
-async def update_events(events_id: int, events_body:events_schema.EventsCreate ):
-    return events_schema.Events(id=events_id, **events_body.dict())
-
-@router.delete('/events/{events_id}', response_model=None)
-async def delete_events(events_id: int):
-    return
