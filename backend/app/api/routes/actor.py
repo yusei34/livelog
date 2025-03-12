5911e97874bf04eacd4282d42fb4aedef0f91d30
yusei <matsu.yusei3414@gmail.com>
2025-03-13 07:30:35 +0900

    actorテーブルを操作するシンプルなエンドポイントを作成

---
+++ backend/app/api/routes/actor.py
@@ -0,0 +1,49 @@
+import uuid
+from typing import Any
+from fastapi import APIRouter, HTTPException, Query
+from sqlmodel import select
+from ..deps import SessionDep
+from models.models import Actor, ActorCreate, ActorPublic, ActorUpdate, Message
+
+router = APIRouter(prefix="/actors", tags=["actors"])
+
+@router.post("/", response_model=ActorPublic)
+def create_actor(session:SessionDep, actor:ActorCreate):
+    db_actor = Actor.model_validate(actor)
+    session.add(db_actor)
+    session.commit()
+    session.refresh(db_actor)
+    return db_actor
+
+@router.get("/", response_model=list[ActorPublic])
+def read_actors(session:SessionDep, offset: int = 0, limit: int = Query(default=20, le=20)):
+    actors = session.exec(select(Actor).offset(offset).limit(limit)).all()
+    return actors
+
+@router.get("/{actor_id}", response_model=ActorPublic)
+def read_actor(session:SessionDep, actor_id: uuid.UUID):
+    actor = session.get(Actor, actor_id)
+    if not actor:
+        raise HTTPException(status_code=404, detail="Actor Not Found")
+    return actor
+
+@router.put("/{actor_id}", response_model=ActorPublic)
+def update_actor(session: SessionDep, actor_id: uuid.UUID, actor: ActorUpdate):
+    db_actor = session.get(Actor, actor_id)
+    if not db_actor:
+        raise HTTPException(status_code=404, detail="Actor Not Found")
+    update_data = actor.model_dump(exclude_unset=True)
+    db_actor.sqlmodel_update(update_data)
+    session.add(db_actor)
+    session.commit()
+    session.refresh(db_actor)
+    return db_actor
+
+@router.delete("/{actor_id}", response_model=Message)
+def delete_actor(session: SessionDep, actor_id: uuid.UUID):
+    actor = session.get(Actor, actor_id)
+    if not actor:
+        raise HTTPException(status_code=404, detail="Actor Not Found")
+    session.delete(actor)
+    session.commit()
+    return Message(message='Actor deleted successfully')
