4b0262386104d8aba34ab5e7085a601bf3cd83ac
yusei <matsu.yusei3414@gmail.com>
2025-03-12 22:02:19 +0900

    crud処理を行うシンプルなエンドポイントを作成した

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from app.api.endpoints import events 
-from app.core.db import create_db_tables
+from api.routes import events 
+from core.db import create_db_tables
 
 app = FastAPI()
 
-@app.on_event('startup')
-def on_startup():
-    create_db_tables()
+# @app.on_event('startup')
+# def on_startup():
+#     create_db_tables()
 
 
 app.include_router(events.router)
