186debe3fbfd00496abcfed00e8ab19408b4c090
yusei <matsu.yusei3414@gmail.com>
2025-03-14 06:56:21 +0900

    expenseテーブルを操作するシンプルなエンドポイントを作成

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from api.routes import events, actor
+from api.routes import events, actors, expenses
 from core.db import create_db_tables
 
 app = FastAPI()
@@ -10,4 +10,5 @@ app = FastAPI()
 
 
 app.include_router(events.router)
-app.include_router(actor.router)
+app.include_router(actors.router)
+app.include_router(expenses.router)
