1cac037373ad8d78f5de2d52cd40fc84d3eef12b
yusei <matsu.yusei3414@gmail.com>
2025-05-22 23:52:12 +0900

    Actor登録を実装中#2

--- frontend/src/app/events/form/action.js
+++ frontend/src/app/events/form/action.js
@@ -1,17 +1,19 @@
-'use server';
-import { postEvent } from '@/lib/fetchEvents';
-import { fetchActorsByIds } from '@/lib/fetchActors';
+"use server";
+import { postEvent } from "@/lib/fetchEvents";
+import { fetchActorsByIds } from "@/lib/fetchActors";
+import { useSearchParams } from "next/navigation";
 
 export async function registerEvent(prevState, formData) {
-  const title = formData.get('title');
-  const venue = formData.get('venue');
-  const event_date = formData.get('event_date');
-  const actor_ids = [] //データの取得方法について検討
+  const searchParams = useSearchParams();
+  const title = formData.get("title");
+  const venue = formData.get("venue");
+  const event_date = formData.get("event_date");
+  const actor_ids = searchParams.getAll("actor_ids");
 
   if (!title || !venue || !event_date) {
-    return { message: 'イベント名,会場,開催日は必須入力です。' };
+    return { message: "イベント名,会場,開催日は必須入力です。" };
   }
 
   const event = { title, venue, event_date };
-  return await postEvent(event, actor_ids)
+  return await postEvent(event, actor_ids);
 }
