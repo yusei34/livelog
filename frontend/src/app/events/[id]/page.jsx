3d41d78e0745a1ba907bff09e4b617ea6a44b632
yusei <matsu.yusei3414@gmail.com>
2025-06-19 06:27:31 +0900

    EventCardおよびEventListItemに詳細ページへのLinkを設定

--- frontend/src/app/events/[id]/page.jsx
+++ frontend/src/app/events/[id]/page.jsx
@@ -2,55 +2,56 @@
 
 import React, { use, useEffect, useState } from "react";
 import { fetchEventById } from "@/lib/api/fetchEvents";
+import Link from 'next/link'
+import { ArrowLeft } from 'lucide-react';
+import { Button } from '@headlessui/react'
+
 
 const EventDetailPage = ({ params }) => {
   const [event, setEvent] = useState(null); // 初期値は null
   const id = params.id;
 
-//   useEffect(() => {
-//     fetchEventById(params.id)
-//       .then((data) => {
-//         setEvent(data); // データをセット
-//       })
-//       .catch((error) => {
-//         console.error("イベント取得失敗:", error);
-//       });
-//   }, [params.id]);
-
   useEffect(() => {
-    fetchEventById(id)
-      .then(setEvent)
-      }, [id]);
-
+    fetchEventById(id).then(setEvent);
+  }, [id]);
 
   // データがまだない場合
   if (!event) return <div>読み込み中...</div>;
 
   return (
     <>
-       <div className="p-4">
-      <h1 className="text-2xl font-bold">{event.title}</h1>
-      <p className="mt-2">会場: {event.venue}</p>
-      <p>開催日: {event.event_date}</p>
+      <div className="p-4 bg-white">
+        <header className="flex gap-5">
+        <Button className='border  border-gray-200 rounded data-hover:bg-gray-200 data-hover:data-active:bg-gray-300'>
+            <Link href='/events'>
+            
+            <ArrowLeft className="h-10 w-auto"/>
+            </Link>
+        </Button>
+        <h1 className="text-4xl font-bold">{event.title}</h1>
+        </header>
+        
+        <p className="mt-2">会場: {event.venue}</p>
+        <p>開催日: {event.event_date}</p>
 
-      <h2 className="mt-4 font-semibold">出演者</h2>
-      <ul className="list-disc list-inside">
-        {event.actors.map((actor) => (
-          <li key={actor.id}>{actor.name}</li>
-        ))}
-      </ul>
-
-      <h2 className="mt-4 font-semibold">経費</h2>
-      {event.expenses.length === 0 ? (
-        <p>なし</p>
-      ) : (
+        <h2 className="mt-4 font-semibold">出演者</h2>
         <ul className="list-disc list-inside">
-          {event.expenses.map((expense) => (
-            <li key={expense.id}>{expense.name}</li>
+          {event.actors.map((actor) => (
+            <li key={actor.id}>{actor.name}</li>
           ))}
         </ul>
-      )}
-    </div>
+
+        <h2 className="mt-4 font-semibold">経費</h2>
+        {event.expenses.length === 0 ? (
+          <p>なし</p>
+        ) : (
+          <ul className="list-disc list-inside">
+            {event.expenses.map((expense) => (
+              <li key={expense.id}>{expense.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </>
   );
 };
--- frontend/src/components/EventListItem.jsx
+++ frontend/src/components/EventListItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { MapPin, Calendar, ChevronRight } from "lucide-react";
 
 const EventListItem = ({ event }) => {
@@ -9,21 +10,21 @@ const EventListItem = ({ event }) => {
   });
   return (
     <>
-      <div className="flex items-center justify-between rounded-b-lg border-b border-gray-200 p-4  hover:bg-gray-50 hover:transition-colors hover:shadow-md">
-        <div className="space-y-1">
-          <div className="px-2">
-            <h2 className="font-medium text-lg">{event.title}</h2>
+        <Link href={`/events/${event.id}`} className="flex items-center justify-between rounded-b-lg border-b border-gray-200 p-4  hover:bg-gray-50 hover:transition-colors hover:shadow-md">
+          <div className="space-y-1">
+            <div className="px-2">
+              <h2 className="font-medium text-lg">{event.title}</h2>
+            </div>
+            <div className="px-2 flex items-center text-sm text-gray-700 text-muted-foreground">
+              <Calendar className="h-3.5 w-3.5 mr-1" />
+              <span>{date}</span>
+              <span className="mx-2">•</span>
+              <MapPin className="h-3.5 w-3.5 mr-1" />
+              <span>{event.venue}</span>
+            </div>
           </div>
-          <div className="px-2 flex items-center text-sm text-gray-700 text-muted-foreground">
-            <Calendar className="h-3.5 w-3.5 mr-1" />
-            <span>{date}</span>
-            <span className="mx-2">•</span>
-            <MapPin className="h-3.5 w-3.5 mr-1" />
-            <span>{event.venue}</span>
-          </div>
-        </div>
-        <ChevronRight className="h-5 w-5 text-muted-foreground" />
-      </div>
+          <ChevronRight className="h-5 w-5 text-muted-foreground" />
+        </Link>
     </>
   );
 };
