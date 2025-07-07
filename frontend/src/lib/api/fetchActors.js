de709cafc2d2725e1ab1831ba0bd466afd775b2a
yusei <matsu.yusei3414@gmail.com>
2025-07-07 22:37:28 +0900

    登録フォームのモーダル化完了

--- frontend/src/lib/api/fetchActors.js
+++ frontend/src/lib/api/fetchActors.js
@@ -1,14 +1,22 @@
 // src/lib/api/fetchActors.js
 import axios from 'axios';
 
-export async function fetchActors({ query = '' } = {}) {
+export async function fetchActors(skip=0, limit=20) {
   try {
-    const response = await axios.get('http://127.0.0.1:8000/actors/', {
-      params: query ? { name: query } : {},
-    });
-    return response.data.data; // ← FastAPIの "data" フィールドを展開
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/actors?skip=${skip}&limit=${limit}`);
+    return res.data.data; 
   } catch (error) {
-    console.error('Failed to fetch actors:', error);
-    return [];
+    console.error('アーティストの取得失敗:', error);
+    throw new Error('アーティストの取得に失敗しました');
+  }
+}
+
+export async function fetchQueryActors(q) {
+  try {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/actors?q=${q}`);
+    return res.data.data; 
+  } catch (error) {
+    console.error('アーティスト取得失敗:', error);
+    throw new Error('アーティスト情報の取得に失敗しました');
   }
 }
