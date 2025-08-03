b84ef9c2836504f664b155bf14c7dcf412a47914
yusei <matsu.yusei3414@gmail.com>
2025-08-03 20:38:29 +0900

    ページ数を表示した

--- frontend/src/app/events/page.jsx
+++ frontend/src/app/events/page.jsx
@@ -21,37 +21,52 @@ export default function EventsPage() {
     const fetchEvent = async () => {
       const skip = (page - 1) * PAGE_SIZE;
       const res = await fetchAllEvents(skip, PAGE_SIZE);
-      setEvents(res);
-      setTotal(events.length());
+      setEvents(res.data);
+      setTotal(res.total);
     };
     fetchEvent();
   }, [page]);
 
-  // const totalPages = Math.ceil(total / PAGE_SIZE);
+  const totalPages = Math.ceil(total / PAGE_SIZE);
 
   return (
     <>
-      <div className="flex flex-col p-4 m-8 space-y-6">
-        <h1 className="text-2xl font-bold">ライブ一覧</h1>
+      <div className="min-h-screen bg-gradient-to-br from-green-50 via-white to-emerald-50">
+        <div className="flex flex-col space-y-6">
+          {/* title */}
+          <div className="bg-gradient-to-r from-green-300 to-orange-300">
+            <div className="w-max flex flex-col border p-12 mx-24 gap-3">
+              <div className="text-white text-4xl font-bold tracking-tight">
+                参戦ライブ一覧
+              </div>
+              <div className="bg-emerald-200 rounded-xl border border-white text-xl flex flex-col items-center py-4 gap-4">
+                <span>総参戦数</span>
+                <span>{total}件</span>
+              </div>
+            </div>
+          </div>
+          
+          <div className="p-8">
+            <SearchBar className="justify-self-center" />
+          </div>
 
-        <SearchBar className="justify-self-center" />
+          <div className="mx-12">
+            <Pagination
+              page={page}
+              totalPages={totalPages}
+              onPageChange={setPage}
+            />
+          </div>
 
-        <div className="mx-12">
-          <Pagination
-            page={page}
-            // totalPages={totalPages}
-            onPageChange={setPage}
-          />
-        </div>
-
-        <div className="p-2 mt-4 overflow-y-auto transition flex flex-col-reverse rounded-3xl ">
-          {events.length === 0 ? (
-            <p>イベントがありません</p>
-          ) : (
-            events.map((event) => (
-              <EventListItem key={event.id} event={event} />
-            ))
-          )}
+          <div className="p-2 mt-4 overflow-y-auto transition flex flex-col-reverse rounded-3xl ">
+            {events.length === 0 ? (
+              <p>イベントがありません</p>
+            ) : (
+              events.map((event) => (
+                <EventListItem key={event.id} event={event} />
+              ))
+            )}
+          </div>
         </div>
       </div>
     </>
--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-
 const Pagination = ({ page, totalPages, onPageChange }) => {
   return (
-    <nav className="flex gap-2 mt-4">
+     <nav className="flex gap-2 mt-4">
       <button
         onClick={() => onPageChange(page - 1)}
-        // disabled={page === 1}
+        disabled={page === 1}
         className="px-3 py-1 border rounded"
       >
         前へ
       </button>
-      
-        {/* <button
+      {Array.from({length: totalPages}, (_, i) => (
+        <button
+          key={i+1}
           onClick={() => onPageChange(i + 1)}
-          className={`px-3 py-1 border rounded `}
+          className={`px-3 py-1 border rounded ${page === i+1 ? "bg-green-200 font-bold" : ""}`}
         >
-        </button> */}
-   
+          {i + 1}
+        </button>
+      ))}
       <button
         onClick={() => onPageChange(page + 1)}
-        // disabled={page === totalPages || totalPages === 0}
+        disabled={page === totalPages || totalPages === 0}
         className="px-3 py-1 border rounded"
       >
         次へ
