d48127a2a4da9f4c0e54dbf964fd35f0bce990e1
yusei <matsu.yusei3414@gmail.com>
2025-08-14 22:48:17 +0900

    コンファーム機能を実装した。UIの改善要

--- frontend/src/components/DeleteEvent.jsx
+++ frontend/src/components/DeleteEvent.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { deleteEvent } from "../lib/api/deleteEvent";
 import { toast } from "sonner";
@@ -8,8 +8,9 @@ import { Button } from "@headlessui/react";
 import { Trash2 } from "lucide-react";
 const DeleteEvent = ({ id }) => {
   const router = useRouter();
+  const [showConfirm, setShowConfirm] = useState(false);
 
-  const Delete = async (id) => {
+  const handleDelete = async () => {
     try {
       await deleteEvent(id);
       toast.success("削除しました");
@@ -21,12 +22,38 @@ const DeleteEvent = ({ id }) => {
 
   return (
     <>
+      {/* 削除ボタン */}
       <Button
         className="p-2 bg-white/20 hover:bg-white/30 text-white border-white/20 rounded"
-        onClick={() => Delete(id)}
+        onClick={() => setShowConfirm(true)}
       >
         <Trash2 className="h-6 w-6" />
       </Button>
+
+      {/* 確認モーダル */}
+      {showConfirm && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50">
+          <div className="bg-white pt-4 grid-cols-2 rounded-lg shadow-lg w-100 h-40">
+            <div className="col-span-2 m-4 text-gray-800 text-center font-semibold">
+              このイベントを削除してもよろしいですか？
+            </div>
+
+              <Button
+                className=" col-span-1 w-40 px-4 text-blue-500 py-2 bg-gray-200 rounded hover:bg-gray-300"
+                onClick={() => setShowConfirm(false)}
+              >
+                キャンセル
+              </Button>
+              <Button
+                className="col-span-1 w-40 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                onClick={handleDelete}
+              >
+                削除
+              </Button>
+      
+          </div>
+        </div>
+      )}
     </>
   );
 };
